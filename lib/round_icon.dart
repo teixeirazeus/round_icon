eaf567f56fe37408387a27ad74598e319b1d10c4
teixeirazeus <teixeira.zeus@gmail.com>
2022-09-12 15:16:44 -0300

    function to widget

--- lib/round_icon.dart
+++ lib/round_icon.dart
@@ -3,18 +3,35 @@ library round_icon;
 import 'package:flutter/material.dart';
 
 /// Creates a round icon with a background color.
-Widget roundIcon(IconData icon, Color backgroundColor, Color iconColor,
-    {double size = 24.0, double padding = 5.0}) {
-  return Container(
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8), color: backgroundColor),
-    child: Padding(
-      padding: EdgeInsets.all(padding),
-      child: Icon(
-        icon,
-        color: iconColor,
-        size: size,
+// ignore: must_be_immutable
+class RoundIcon extends StatelessWidget {
+  IconData icon;
+  Color backgroundColor;
+  Color iconColor;
+  double size;
+  double padding;
+  RoundIcon({
+    Key? key,
+    required this.icon,
+    required this.backgroundColor,
+    required this.iconColor,
+    this.size = 24.0,
+    this.padding = 8.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8), color: backgroundColor),
+      child: Padding(
+        padding: EdgeInsets.all(padding),
+        child: Icon(
+          icon,
+          color: iconColor,
+          size: size,
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
